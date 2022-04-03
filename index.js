265b4e3f7ea54dd2273b243ffd0e6db0f2726428
Daniel Becker <daniel.becker000@gmail.com>
2022-04-03 09:44:52 -0400

    commit tree cc03f01cd4feb12045dff0092f5327efd210a0f0

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Write your solution here!
+// built and burned 1x 4/1
+
+ let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat (name) {
+    cats.push(name); 
+    return cats;
+}
+//  ^was confused because it doesn't take an array as an argument
+// ^uses "cats" array 
+
+function destructivelyPrependCat (name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats; 
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(); 
+    return cats;
+}
+
+function appendCat(name) {
+ let newArray= [...cats];
+ newArray.push(name);
+ return newArray;
+}
+
+// THIS MORE COMPACT FUNCTION DOESN'T WORK
+// function appendCat(name) {
+//     let newArray= [...cats].push(name);
+//     return newArray;
+//    }
+
+function prependCat(name) {
+    let newArray= [...cats];
+    newArray.unshift(name);
+    return newArray;
+}
+
+function removeLastCat() {
+    let newArray= [... cats];
+    newArray.pop();
+    return newArray;
+}
+
+function removeFirstCat() {
+    let newArray= [... cats];
+    newArray.shift();
+    return newArray;
+}
